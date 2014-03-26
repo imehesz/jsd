6812789f1fba875f8aa37acb9440a6a1184ccb7a
Imre Mehesz <imehesz@gmail.com>
2014-03-25 21:50:48 -0400

    moving along

--- app/webApp.js
+++ app/webApp.js
@@ -1,5 +1,22 @@
 var webApp = angular.module("webApp",[]);
 
-webApp.controller("AppController", function(){
+webApp.controller("AppController", function($scope){
   // angular goodness will come here ...
+  $scope.lessons = [];
+  $scope.activeLesson = null;
+  $scope.lessonNotFound = "";
+  
+  if (typeof JSD != "undefined") {
+    $scope.lessons = JSD.lessons;
+  }
+  
+  $scope.loadLesson = function(lName) {
+    console.log("lName", lName);
+    var lesson = _.find($scope.lessons,function(rw){ return rw.name == lName });
+    if (lesson) {
+      $scope.activeLesson = lesson;
+    } else {
+      $scope.lessonNotFound = "Lesson Not Found :/";
+    }
+  }
 });
