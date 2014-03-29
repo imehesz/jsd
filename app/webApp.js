a3e09a0b7d8cad2163e920e0177360b5c067d43c
Imre Mehesz <imehesz@gmail.com>
2014-03-28 22:30:16 -0400

    moved tests around, still not sure if I like the format :/

--- app/webApp.js
+++ app/webApp.js
@@ -1,15 +1,46 @@
 var webApp = angular.module("webApp",[]);
 
-webApp.directive("webappTestDirective", function(){
+webApp.directive("webappTestsDirective", function($sce){
   return {
     restrict: "C",
     replace: true,
     scope:{
-      ngTest: "="
+      ngTests: "=",
+      openTests: "="
     },
-    template: "<div>{{ngTest.name}}</div>",
+    template: '<div>\
+                  <h3>{{ngTests[activeTestNum].label}}</h3>\
+                    <div ng-bind-html="parsedCode">{{parsedCode}}</div>\
+                    <div ng-show="ngTests.length>1">\
+                      <button type="button" ng-click="prevTest()" ng-disabled="activeTestNum==0"><< Prev</button>\
+                      <button type="button" ng-click="nextTest()" ng-disabled="activeTestNum==ngTests.length-1">>> Next</button>\
+                      <button type="button" ng-click="openTests=false">Close Tests</button>\
+                    </div>\
+              </div>',
     link: function(scope,element,attr) {
+      scope.activeTestNum = 0;
+      scope.activeTestContent = "";      
       // directive logic will come here ...
+      
+      scope.$watch("activeTestNum", function(o,n){
+        var rawCode = "";
+        rawCode = typeof scope.ngTests != "undefined" ? scope.ngTests[scope.activeTestNum].code : "";
+        
+        if (rawCode) {
+          rawCode = rawCode.replace(/\[br\]/g,'<br>');
+        }
+        scope.parsedCode = $sce.trustAsHtml(rawCode);
+      });
+  
+      scope.nextTest = function() {
+        if (scope.ngTests) {
+          if (scope.activeTestNum != scope.ngTests.length) scope.activeTestNum++;
+        }
+      }
+      
+      scope.prevTest = function() {
+        if(scope.activeTestNum > 0) scope.activeTestNum--;
+      }
     }
   }
 });
@@ -21,15 +52,12 @@ webApp.controller("AppController", function($scope, $sce){
   $scope.activePageContent = "";
   $scope.activePageNum = 0;
   $scope.lessonNotFound = "";
-  $scope.activeTestNum = 0;
-  $scope.activeTestContent = "";
   
   if (typeof JSD != "undefined") {
     $scope.lessons = JSD.lessons;
   }
   
   $scope.loadLesson = function(lName) {
-    console.log("lName", lName);
     $scope.activePageNum = 0;
     var lesson = _.find($scope.lessons,function(rw){ return rw.name == lName });
     if (lesson) {
@@ -59,14 +87,4 @@ webApp.controller("AppController", function($scope, $sce){
       $scope.activePageContent = $sce.trustAsHtml($scope.activeLesson.pages[$scope.activePageNum].content);
     }
   }
-  
-  $scope.nextTest = function() {
-    if ($scope.activeLesson && $scope.activeLesson.tests) {
-      if ($scope.activeTestNum != $scope.activeLesson.tests.length) $scope.activeTestNum++;
-    }
-  }
-  
-  $scope.prevTest = function() {
-    if($scope.activeTestNum > 0) $scope.activeTestNum--;
-  }  
 });
